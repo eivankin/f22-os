830890c036ba6479dec659947328cb2f1191a21d
Evgenij Ivankin <e.ivankin@innopolis.university>
2022-09-11 17:15:27 +0300

    add missing double quotation

--- week02/ex2.c
+++ week02/ex2.c
@@ -2,16 +2,20 @@
 
 #define TERMINAL_CHAR '.'
 #define EOL '\n'
+#define QUOTE '"'
 
 void readAndPrintUntil() {
 	// Recursion is more beautiful than C arrays
 	char input = getchar();
-	if (input == TERMINAL_CHAR || input == EOL)
+	if (input == TERMINAL_CHAR || input == EOL) {
+		putchar(QUOTE);
 		return;
+	}
 	readAndPrintUntil();
 	putchar(input);
 }
 
 int main() {
 	readAndPrintUntil();
+	putchar(QUOTE);
 }
